8b1a55390164d78722b3d7c56bf90a13db353da2
Suraj Gopal <surajcancode@gmail.com>
2023-04-14 07:26:43 -0400

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts("destroying all clicks...")
+Click.destroy_all
+puts("still working...")
+puts("...")
+puts("rebuilding db")
+Click.create!(name: "Flatiron Building", address: "175 5th Ave, New York, NY 10010")
+Click.create!(name: "The Getty Center", address: "200 Getty Center Dr, Los Angeles, CA 90049, USA")
+Click.create!(name: "Livrustkammaren", address: "Slottsbacken 3, 111 30 Stockholm, Sweden")
+Click.create!(name: "Signal Iduna Park", address: "Strobelallee 50, 44139 Dortmund, Germany")
+puts("finished!")
